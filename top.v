1b9877c90d033712ab397170ee12e6fcdca1c21b
SON DONG HAN <20221993@edu.hanbat.ac.kr>
2026-05-18 15:36:20 +0900

    refactor
    top.vinput_manager를 감싸는 실물 확인용 top
    LED 출력 연결:led[0] = key_valid
    led[1] = hash_key
    led[2] = admin_key
    led[3] = clear_key
    led[6:4] = input_count
    led[7] = reset_n
    
    input_manager.qsfTOP_LEVEL_ENTITY top으로 변경
    top.v를 소스 목록에 추가
    
    pin_assignments.qsfled[7:0] 핀 할당 추가

---
+++ top.v
@@ -0,0 +1,39 @@
+module top (
+    input  wire        clk,
+    input  wire        reset_n,
+    input  wire [15:0] button_in,
+    output wire [7:0]  led
+);
+
+    wire        key_valid;
+    wire [3:0]  key_data;
+    wire        hash_key;
+    wire        admin_key;
+    wire        clear_key;
+    wire [2:0]  input_count;
+    wire [15:0] pass_buffer;
+
+    input_manager #(
+        .CLK_FREQ_HZ(1000000),
+        .DEBOUNCE_MS(20)
+    ) u_input_manager (
+        .clk(clk),
+        .reset_n(reset_n),
+        .button_in(button_in),
+        .key_valid(key_valid),
+        .key_data(key_data),
+        .hash_key(hash_key),
+        .admin_key(admin_key),
+        .clear_key(clear_key),
+        .input_count(input_count),
+        .pass_buffer(pass_buffer)
+    );
+
+    assign led[0] = key_valid;
+    assign led[1] = hash_key;
+    assign led[2] = admin_key;
+    assign led[3] = clear_key;
+    assign led[6:4] = input_count;
+    assign led[7] = reset_n;
+
+endmodule
